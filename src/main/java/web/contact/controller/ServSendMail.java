8cecace8fd68a72f4844310312197a628eb93d33
Peter1 <40471207@gm.nfu.edu.tw>
2022-02-27 00:00:52 +0800

    Modify: ServInsertController、ServSelectAllController、ServiceDAO、CommonUtil、backstage(html、css、js) Create: ServSendMail、dataTables.bookstrap.min.css、contact.jsp

--- src/main/java/web/contact/controller/ServSendMail.java
+++ src/main/java/web/contact/controller/ServSendMail.java
@@ -1,5 +1,6 @@
 package web.contact.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,7 +8,6 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,28 +18,19 @@ import web.contact.entity.impl.ServiceBean;
 import web.contact.service.ServServiceInterface;
 
 @Controller
-public class ServSelectAllController {
+public class ServSendMail {
 
 	@Autowired
 	private ServServiceInterface service;
-	
-	@ResponseBody
-	@PostMapping(value = "/backstage/ServiceAllData.controller")
-	public String ServSelectAll() {
-		System.out.println(1);
-		List<ServiceBean> list = service.select();
-		Gson gson = new Gson();
 
-		return gson.toJson(list);
+	@ResponseBody
+	@PostMapping(value = "/backstage/ServSendMail.controller")
+	public String ServSelectAll(@RequestBody(required = false) HashMap<String, String> req, HttpSession session) {
+		
+		ServiceBean bean = new ServiceBean();
+		bean.setTaskNo(Integer.valueOf(req.get("taskNo")));
+		bean = service.sendEmail(bean, req.get("subject"), req.get("replyContent"));
+		Gson gson = new Gson();
+		return gson.toJson(bean);
 	}
-	
-//	@PostMapping(value = "/ServiceAllData.do")
-//	public String ServSelectll(@RequestBody(required = false) ServiceBean bean, HttpSession session) {
-//		System.out.println(1);
-//		List<ServiceBean> list = service.select();
-//		Gson gson = new Gson();
-//
-//		return gson.toJson(list);
-//	
-//}
 }
